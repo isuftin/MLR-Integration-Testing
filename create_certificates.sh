755edbade98e042db16b9f292e02d7f928f29157
Ivan Suftin <isuftin@gmail.com>
2018-12-12 16:59:59 -0600

    Update create certificates shell script against shellcheck

--- create_certificates.sh
+++ create_certificates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-openssl genrsa -out $DIR/ssl/wildcard.key 2048
-openssl req -nodes -newkey rsa:2048 -keyout $DIR/ssl/wildcard.key -out $DIR/ssl/wildcard.csr -subj "/C=US/ST=Wisconsin/L=Middleon/O=US Geological Survey/OU=WMA/CN=*"
-openssl x509 -req -days 9999 -in $DIR/ssl/wildcard.csr -signkey $DIR/ssl/wildcard.key -out $DIR/ssl/wildcard.crt
+openssl genrsa -out "$DIR/ssl/wildcard.key" 2048
+openssl req -nodes -newkey rsa:2048 -keyout "$DIR/ssl/wildcard.key" -out "$DIR/ssl/wildcard.csr" -subj "/C=US/ST=Wisconsin/L=Middleon/O=US Geological Survey/OU=WMA/CN=*"
+openssl x509 -req -days 9999 -in "$DIR/ssl/wildcard.csr" -signkey "$DIR/ssl/wildcard.key" -out "$DIR/ssl/wildcard.crt"
