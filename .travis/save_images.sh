239cdb13b4a6ca0df92ab3865159da140d7cced4
Suftin, Ivan <isuftin@usgs.gov>
2019-02-08 17:41:20 -0600

    test

--- .travis/save_images.sh
+++ .travis/save_images.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
 ls -al $HOME/docker/
 
 for img in $(docker image ls --format "{{ .Repository }}:{{ .Tag }}"); do
   echo "Saving ${img} to ${HOME}/docker/${img//[\/:]/.}.tar"
-  docker save $img -o "$HOME/docker/${img//[\/:]/.}.tar"
+  docker save $img -o "${HOME}/docker/${img//[\/:]/.}.tar"
 done
 
 if [ ! -f "$HOME/docker/jmeter.tar" ]; then
   docker-compose -f docker-compose-jmeter-servers.yml build
-  docker save "jmeter-base:latest" -o "$HOME/docker/jmeter.tar"
+  docker save "jmeter-base:latest" -o "${HOME}/docker/jmeter.tar"
 fi
